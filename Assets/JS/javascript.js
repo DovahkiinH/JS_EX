df5b2f895a51a7512ec4197fca3a7c51fecb57b6
DovahkiinH <loick.caplier@gmail.com>
2023-09-07 09:09:57 +0200

    Ex5 JS

--- Assets/JS/javascript.js
+++ Assets/JS/javascript.js
@@ -9,4 +9,11 @@ alert("vous vous appelez" + nom + " " + prenom);
 var nombre1 = window.prompt("Choisissez un premier nombre");
 var nombre2 = window.prompt("Choisissez un deuxième nombre");
 
-alert("Le produit de" + " " + nombre1 + " " + "et" + " " + nombre2 + " " + "est" + " " + nombre1*nombre2)
+alert("Le produit de" + " " + nombre1 + " " + "et" + " " + nombre2 + " " + "est" + " " + nombre1*nombre2);
+
+/* Script degrés Celsius -> Fahrenheit */
+
+var C = window.prompt("Indiquez les degrés Celsius");
+var F = (C*9/5) + 32 ;
+
+alert( C + " " + "degrés Celsius équivaut à " + " " + F + " " + "Fahrenheit");
