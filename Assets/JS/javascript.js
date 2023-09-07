a82a75048d29ea260fb53263be9e0fdfe86c72ad
DovahkiinH <loick.caplier@gmail.com>
2023-09-07 09:02:05 +0200

    Ex 5 Js

--- Assets/JS/javascript.js
+++ Assets/JS/javascript.js
@@ -1,6 +1,12 @@
 /* Script Nom Prénom */
-var reponse1 = window.prompt("Saisissez votre nom");
-var reponse2 = window.prompt("Saisissez votre prénom");
+var nom = window.prompt("Saisissez votre nom");
+var prenom = window.prompt("Saisissez votre prénom");
 
-alert(reponse1 + " " + reponse2);
+alert("vous vous appelez" + nom + " " + prenom);
 
+/* Script Produit de 2 nombre */
+
+var nombre1 = window.prompt("Choisissez un premier nombre");
+var nombre2 = window.prompt("Choisissez un deuxième nombre");
+
+alert("Le produit de" + " " + nombre1 + " " + "et" + " " + nombre2 + " " + "est" + " " + nombre1*nombre2)
